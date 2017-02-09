b3f5e1e0cb18f8f99e30aaa6a99e2da50dcd96cf
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2017-02-09 11:30:12 -0600

    add table output binding

--- EventHubToFunction/index.js
+++ EventHubToFunction/index.js
@@ -1,9 +1,23 @@
-﻿
-
-module.exports = function (context, myEventHubTrigger) {
+﻿module.exports = function (context, myEventHubTrigger) {
     context.log('Node.js eventhub trigger function processed work item', myEventHubTrigger);
 
     context.log("context: " + JSON.stringify(context));
+    
+    if (myEventHubTrigger.Id) {
+        var ticks = (new Date()).getTime();
 
-    context.done();
+        var tableResult = {
+            PartitionKey: myEventHubTrigger.Id,
+            RowKey: ticks,
+            Data: myEventHubTrigger
+        };
+
+        context.done(null, tableResult);
+    }
+    else {
+        context.log("myEventHubTrigger did not have an id...");
+        context.log("myEventHubTrigger: " + JSON.stringify(myEventHubTrigger));
+
+        context.done();
+    }
 };
