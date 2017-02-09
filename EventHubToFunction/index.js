bc1b6abdb9d4f87640a411b87012a319d62ee009
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2017-02-09 10:37:18 -0600

    add event hub function

---
+++ EventHubToFunction/index.js
@@ -0,0 +1,9 @@
+﻿
+
+module.exports = function (context, myEventHubTrigger) {
+    context.log('Node.js eventhub trigger function processed work item', myEventHubTrigger);
+
+    context.log("context: " + JSON.stringify(context));
+
+    context.done();
+};
